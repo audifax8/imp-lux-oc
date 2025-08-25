22a9bada2bef644d71f0be0b55d240711b89c0ac
Javier Gómez <franciscog@thisisyr.com>
2025-08-25 08:20:25 -0500

    Render RTR

--- src/components/Model/Model3.tsx
+++ src/components/Model/Model3.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 
-import { useConfigureImg, useIsCustomizerOpen, useIsMobile } from '@/state/ui';
+import { apis } from '@/libs/apis';
+
+import { useConfigureImg, useIsCustomizerOpen, useIsMobile, useToken } from '@/state/ui';
+import { useRTRAPIReady, useRTRDisabled, useRTRError } from '@/state/rtr';
 
 import { IConfigureAPI } from '@/declarations/interfaces';
 
@@ -18,15 +21,34 @@ export type IModelProps = {
 
 export default React.memo(function Model({ corePromise }: IModelProps) {
   corePromise.read();
+  const [rtrStarted, setRTRStarted] = useState(false);
   const [img] = useConfigureImg();
   const imageData = getImgData();
+  const [token] = useToken();
   const [isCustomizerOpen, setIsCustomizerOpen] = useIsCustomizerOpen();
   const [isMobile] = useIsMobile();
+  const [rtrDisabled] = useRTRDisabled();
+  const [rtrError] = useRTRError();
+  const [rtrAPIReady] = useRTRAPIReady();
+
+  useEffect(() => {
+    if (rtrDisabled) {
+      return;
+    }
+    if (rtrAPIReady && token && rtrStarted) {
+      apis.rtrAPI.setId(token);
+    }
+    if (rtrAPIReady && token && !rtrStarted) {
+      apis.rtrAPI?.init(token);
+      setRTRStarted(true);
+    }
+  }, [rtrDisabled, rtrAPIReady, token, rtrError, rtrStarted]);
 
   return (img && 
     <section className='yr-model'>
+      <div id='viewer' className={clsx('yr-model__rtr', { 'yr-model__hidden': !rtrAPIReady })}></div>
       <picture
-        className={clsx('yr-model__placeholder', 'yr-image', { 'yr-model__hidden': false }, { 'yr-customizer-open': (isCustomizerOpen && isMobile) })}
+        className={clsx('yr-model__placeholder', 'yr-image', { 'yr-model__hidden': rtrAPIReady }, { 'yr-customizer-open': (isCustomizerOpen && isMobile) })}
         onClick={() => {
           if (!isMobile) {
             return;
